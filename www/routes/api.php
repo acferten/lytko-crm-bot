3384ba41103454dd793cda12cb8d206b4167de28
acferten <gowiththeirname@gmail.com>
2024-05-06 00:30:56 +0700

    remove test route and return type

--- www/routes/api.php
+++ www/routes/api.php
@@ -4,5 +4,3 @@ use App\Http\Telegram\Controllers\TelegramWebhookController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/telegram-webhook', TelegramWebhookController::class);
-
-Route::apiResource('test', \App\Http\Controllers\TestController::class);
