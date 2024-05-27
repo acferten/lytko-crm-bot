eaf22dbd9b7188db281c5dbbf1c202b38322e822
acferten <gowiththeirname@gmail.com>
2024-05-27 22:39:43 +0700

    auth required

--- www/src/Domain/Order/Telegram/Commands/GetOrderInfoCommand.php
+++ www/src/Domain/Order/Telegram/Commands/GetOrderInfoCommand.php
@@ -4,6 +4,7 @@ namespace Domain\Order\Telegram\Commands;
 
 use Domain\Order\Models\Order;
 use Domain\Order\Telegram\Messages\OrderCardMessage;
+use Domain\Shared\Models\User;
 use SergiX44\Nutgram\Handlers\Type\Command;
 use SergiX44\Nutgram\Nutgram;
 
@@ -15,6 +16,14 @@ class GetOrderInfoCommand extends Command
 
     public function handle(Nutgram $bot, int $id): void
     {
+        $employee = User::where('telegram_id', $bot->userId())->first();
+
+        if (is_null($employee) || $employee->hasRole('customer')) {
+            $this->menuText('🚫 У Вас нет доступа к этой команде.')->showMenu();
+
+            return;
+        }
+
         $order = Order::findOrFail($id);
 
         OrderCardMessage::send($order, $bot->userId());
